f5b3418e4dfd87997836ca980172bfab4838042d
gusals6069 <gusals6069@gmail.com>
2019-02-13 16:07:11 +0900

    complete

--- src/components/TodoListTemplate.js
+++ src/components/TodoListTemplate.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './TodoListTemplate.css';
 
-const TodoListTemplate = ({form, children}) => {
+const TodoListTemplate = ({form, children, Palette}) => {
   return (
     <main className="todo-list-template">
       <div className="title">
         오늘 할 일
       </div>
+      <section className="palette-wrapper">
+        {Palette}
+      </section>
       <section className="form-wrapper">
         {form}
       </section>
